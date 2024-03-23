03eeb09e86171159ec433843201606c1ea97f084
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2024-03-23 19:18:40 +0600

    third commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="p-3 text-gray-900">
+                    <a class="text-blue-500 hover:text-blue-900" href="">{{ __('Standard') }}</a>
+                </div>
+                <div class="p-3 text-gray-900">
+                    <a class="text-blue-500 hover:text-blue-900" href="">{{ __('Premium') }}</a>
                 </div>
             </div>
         </div>
