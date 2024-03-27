0ac4a8171ed818d5e79b3f455857d400686eaa8b
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2024-03-28 00:27:18 +0600

    passing slug to checkout

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -10,22 +10,26 @@ class SubscriptionController extends Controller
 {
     public function index(Request $request)
     {
-        return View('checkout', [
-            'intent' => $request->user()->createSetupIntent(),
-            'slug' => $request->plan
+        return View("checkout", [
+            "intent" => $request->user()->createSetupIntent(),
+            "slug" => $request->plan,
         ]);
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'payment_method' => 'required',
-            'slug' => 'required'
+            "payment_method" => "required",
+            "slug" => "required",
         ]);
 
-        $plan = Plan::where('slug', $request->slug)->get()->first();
+        $plan = Plan::where("slug", $request->slug)
+            ->get()
+            ->first();
 
-        $request->user()->newSubscription('default', $plan->stripe_id)
+        $request
+            ->user()
+            ->newSubscription("default", $plan->stripe_id)
             ->create($request->payment_method);
 
         return back();
